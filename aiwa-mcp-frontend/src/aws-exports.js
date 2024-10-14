a1bb46fe142c49c5c16edff3f5290a6baa5747f4
quila-g1 <100333575+quila-g1@users.noreply.github.com>
2024-10-14 11:01:13 +0900

    Delete aiwa-mcp-frontend/src/aws-exports.js

--- aiwa-mcp-frontend/src/aws-exports.js
+++
@@ -1,8 +0,0 @@
-const awsmobile = {
-    "aws_project_region": 'ap-northeast-2',
-    "aws_cognito_identity_pool_id": '',
-    "aws_user_pools_id": 'ap-northeast-2_qCjWAmKqw',
-    "aws_user_pools_web_client_id": '2lps1llva1t5bkbe5g16qbb2cs',
-}
-
-export default awsmobile;
