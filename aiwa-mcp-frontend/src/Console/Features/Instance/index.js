c196d426b642afb0bd963164463132fc64c99ed6
OZMOHYNSK <ramdepluy2104@gmail.com>
2024-11-25 19:02:07 +0900

    [Fix]: eni 라우팅 추가

--- aiwa-mcp-frontend/src/Console/Features/Instance/index.js
+++ aiwa-mcp-frontend/src/Console/Features/Instance/index.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Instance from './Instance'; // Main EC2 Console page
 import AWS_instance_launch from './AWS_instance_launch'; // AWS subpage
 import GCP_instance_launch from './GCP_instance_launch'; // GCP subpage
+import ENI from './eni/eni';
 
 function InstanceRoutes() {
     return (
@@ -11,6 +12,7 @@ function InstanceRoutes() {
             <Route path="/" element={<Instance />} /> {/* Main EC2 Console */}
             <Route path="aws" element={<AWS_instance_launch />} /> {/* AWS Subpage */}
             <Route path="gcp" element={<GCP_instance_launch />} /> {/* GCP Subpage */}
+            <Route path="/eni" element={<ENI />} /> {/* Main EC2 Console */}
         </Routes>
     );
 }
